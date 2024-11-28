0baeb7dbeea337d4e6cd4fbb47b0970756b46092
Arthur Silva <168222534+thurinkz@users.noreply.github.com>
2024-11-28 17:31:37 -0300

    dorr

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'HTML é uma linguagem de marcação usada para estruturar conteúdo na web.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior rio do mundo?',
+    'O maior rio do mundo em volume de água é o Rio Amazonas.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um array?',
+    'Um array é uma estrutura de dados que armazena uma coleção de valores em uma única variável.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz obrigado em Inglês?',
+    'Obrigado em inglês é thank you.'
+)
